b9bbf1e61fe5ab3c792555c89c2d0c011f7ec101
Nitemare Rulz <rkupeed1313@gmail.com>
2018-05-07 10:37:13 +0100

    Addd model classes
    price and product class and their types

---
+++ ElmTest04/Model/Models/Price.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+   public class Price
+    {
+        public decimal Amt { get; set; }
+        public OfferType OfferType { get; set; }
+    }
+}
---
+++ ElmTest04/Model/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public ProductType Name { get; set; }
+        public Price Price { get; set; }
+
+
+    }
+    public enum ProductType
+    {
+        A,
+        B,
+        c
+    }
+    public enum OfferType
+    {
+        BuyOneGetOne,
+        ThreeForTenEuro,
+        None
+    }
+}
