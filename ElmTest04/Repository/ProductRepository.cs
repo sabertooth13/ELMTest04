d751dc8d6465ea8b84a2900f80590032c02fb3fb
Nitemare Rulz <rkupeed1313@gmail.com>
2018-05-07 10:47:36 +0100

    added product store
    product repository added

---
+++ ElmTest04/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IList<Product> FindAll()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product{Id=1,Name=ProductType.A,Price= new Price{ Amt=20,OfferType=OfferType.BuyOneGetOne} },
+                new Product{Id=2,Name=ProductType.B,Price=new Price{Amt=4,OfferType=OfferType.ThreeForTenEuro } },
+                new Product{Id=3,Name=ProductType.c,Price=new Price{Amt=5,OfferType=OfferType.None }}
+            };
+
+            return products;
+
+
+        }
+    }
+}
